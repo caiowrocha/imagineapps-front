c963ec34cd60e1df006ca378f7788af7a47b5c87
Caio Rocha <rochacaiodev@gmail.com>
2024-08-03 16:11:14 -0300

    feat: add home component and set its routes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `
+    <h1>Home Works!</h1>
+  `
+})
+export default class HomeComponent {
+}
